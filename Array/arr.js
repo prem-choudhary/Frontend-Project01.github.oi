b2f3901df3c086c1e03f89d4a0d5bc83b448eb80
Prem-choudhary <pk5480524@gmail.com>
2025-11-29 11:05:27 +0530

    arr problem

--- Array/arr.js
+++ Array/arr.js
@@ -1,24 +1,18 @@
 // Array literal
 const fruits = ["apple", "banana", "orange"];
-
 // Array constructor (less common)
 const numbers = new Array(1, 2, 3, 4, 5);
 console.log(numbers);
-
 //  print the double age
-
 const ages = [10, 22, 32, 40, 25, 45];
 
 //  this will also work what the output in not array form
 for (var i = 0; i < ages.length; i++) {
   console.log(ages[i] * 2);
 }
-
 // this is the right way to write a code
 const ageDouble = ages.map((age) => age * 2);
 console.log(ageDouble);
-
 //  use filter print age only less than 40
-
 const ageLessThan40 = ages.filter((age) => age <= 40);
 console.log(ageLessThan40);
